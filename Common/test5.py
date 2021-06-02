b81911263aa98c7420c45899b15e3a19509b1435
Kenji Takashima <kenji@revup.com>
2021-06-01 19:40:48 -0700

    Clean-up of repo, added warning to README

--- Common/test5.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-for chunk in a:
-    i = 0
-    l = []
-    while i < len(offsets):
-        l.append({names[i]: np.frombuffer(chunk[offsets[i]: int(offsets[i] + types[i](1).itemsize)], dtype=types[i])[0]})
-        i += 1
-    p.append(l)
-    
