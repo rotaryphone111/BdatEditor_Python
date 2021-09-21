b144c0264e35a7cb842afe600fe901f854e38a82
Kenji Takashima <kenji@revup.com>
2021-09-21 15:46:12 -0700

    Cythonized code to speed up IO

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+
+USE_CYTHON = True
+
+try:
+    import Cython
+except ImportError:
+    USE_CYTHON = False
+
+ext = '.pyx' if USE_CYTHON else '.c'
+
+extensions = [Extension("BdatReader", ["Bdat/BdatReader"+ext]) ,Extension("BdatWriter", ["Bdat/BdatWriter"+ext])]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
 
 setup(
     name='BdatEditor',
-    version='0.0.3a',
+    version='0.0.4a',
     description='Bdat editor python rewrite',
     author='hydra',
     author_email='knjtkshm@gmail.com',
     license='GNU General Public License v3.0',
     packages=find_packages(),
+    ext_modules=extensions,
     entry_points={
         'console_scripts': [
             'bdateditor=Bdat:main',
